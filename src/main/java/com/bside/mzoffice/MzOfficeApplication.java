531561bd36baa259501ae09cae033d9165d25e48
ekgpgdi <dlekgp0423@naver.com>
2025-02-19 15:25:36 +0900

    🎉 init commit

---
+++ src/main/java/com/bside/mzoffice/MzOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.bside.mzoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzOfficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bside/mzoffice/MzOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.mzoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
