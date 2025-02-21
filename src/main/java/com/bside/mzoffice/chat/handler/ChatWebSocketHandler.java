e3d2b80b6480cbe43d7b59e8d8511a1823dfc783
ekgpgdi <dlekgp0423@naver.com>
2025-02-21 22:28:17 +0900

    feat : HandshakeInterceptor 를 통해서 인증 처리 구현

--- src/main/java/com/bside/mzoffice/chat/handler/ChatWebSocketHandler.java
+++ src/main/java/com/bside/mzoffice/chat/handler/ChatWebSocketHandler.java
@@ -4,22 +4,44 @@ import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.stereotype.Component;
 
+import javax.security.sasl.AuthenticationException;
 import java.io.IOException;
 
 @Component
 public class ChatWebSocketHandler extends TextWebSocketHandler {
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
+    public void afterConnectionEstablished(WebSocketSession session) throws AuthenticationException {
         System.out.println("클라이언트 연결됨: " + session.getId());
+
+        Long customerId = (Long) session.getAttributes().get("customerId");
+
+        if (customerId != null) {
+            System.out.println("Authenticated user: " + customerId);
+        } else {
+            try {
+                session.close(CloseStatus.NOT_ACCEPTABLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            throw new AuthenticationException();
+        }
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         System.out.println("받은 메시지: " + message.getPayload());
 
-        // 받은 메시지를 다시 클라이언트(A)에게 전송
-        session.sendMessage(new TextMessage("서버 응답: TEST "));
+        Long customerId = (Long) session.getAttributes().get("customerId");
+
+        if (customerId != null) {
+            System.out.println(customerId + " sent a message: " + message.getPayload());
+
+            // 응답 메시지 전송
+            session.sendMessage(new TextMessage("Hello " + customerId + ", your message: " + message.getPayload()));
+        } else {
+            throw new AuthenticationException();
+        }
     }
 
     @Override
