82af6153f3170633bd38055433b6a40b9bc594f8
ekgpgdi <dlekgp0423@naver.com>
2025-02-23 23:05:58 +0900

    fix : 허용 포트 수정

--- src/main/java/com/bside/mzoffice/common/config/SecurityConfig.java
+++ src/main/java/com/bside/mzoffice/common/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:80"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
