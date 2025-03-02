f854278bf8dadef1e3ce310c3d20cc8ec4e89d77
ekgpgdi <dlekgp0423@naver.com>
2025-03-02 11:15:52 +0900

    fix

--- src/main/java/com/bside/mzoffice/common/config/SecurityConfig.java
+++ src/main/java/com/bside/mzoffice/common/config/SecurityConfig.java
@@ -66,8 +66,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost", "http://172.30.1.45", "ws://localhost", "ws://172.30.1.45", "http://mz-office.s3-website.kr.object.ncloudstorage.com", "ws://mz-office.s3-website.kr.object.ncloudstorage.com"));
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
